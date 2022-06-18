e7e762e745c7d43256851b7370ce1d3902f13d3e
Javier <javierlozano2001@hotmail.com>
2022-06-18 01:16:53 -0500

    Revert "Revert "Nombre y documento""
    This reverts commit 1c848aa828ed8a6bfa94da08f8fb1b9844ef2d81.

--- database/migrations/2014_10_12_000001_create_users_table.php
+++ database/migrations/2014_10_12_000001_create_users_table.php
@@ -17,12 +17,13 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('doc');
-            $table->string('sede');
+            $table->bigInteger('sede')->unsigned();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->foreign('sede')->references('IdSede')->on('Sedes')->onDelete('cascade');
         });
     }
 
